85043efaac65905132b9b1004323c7e7a21a2b7b
lethiciaKurten <104856512+lethiciaKurten@users.noreply.github.com>
2024-05-03 09:49:11 -0300

    .

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-15T23:59:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-03-22T23:59:00");
+const tempoObjetivo2 = new Date("2024-05-31T00:00:00");
+const tempoObjetivo3 = new Date("2024-08-18T00:00:00");
+const tempoObjetivo4 = new Date("2024-08-22T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
